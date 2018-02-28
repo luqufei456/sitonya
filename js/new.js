f23fdd34d81322422656d3cad257a0eb196ef93b
yiran <35801786+luqufei456@users.noreply.github.com>
2018-02-28 18:35:08 +0800

    修改js

--- js/new.js
+++ js/new.js
@@ -1,33 +1,68 @@
 $(function () {
-    var error_why = false;
-    var error_use = false;
 
-    //验证需求原因是否为空
-    function check_why() {
-        var val = $('#text_put').val();
-        if (val == '') {
-            error_why = true;
+    $('#choose_id').change(function () {
+        var choose = $('#choose_id').val();
+        if(choose == '申请库权限'){
+            $('#table_id').hide();
+        }
+        else{
+            $('#table_id').show();
+        }
+    });
+
+    var error_group_name = false;
+    var error_group_abb = false;
+    var error_person_name = false;
+    var error_base_name = false;
+
+    //验证组名字是否为空
+    function check_group_name() {
+        var val = $('#group_id').val();
+        if(val == ''){
+            error_group_name = true;
             return;
         }
     }
 
-    //验证机器用途是否选择
-    function check_use() {
-        var val = $('#use_box').val();
-        if (val == '选择机器用途') {
-            error_use = true;
+    //验证组缩写是否为空
+    function check_group_abb() {
+        var val = $('#abb_id').val();
+        if(val == ''){
+            error_group_abb = true;
+            return;
+        }
+    }
+
+    //验证负责人是否为空
+    function check_person_name() {
+        var val = $('#name_id').val();
+        if(val == ''){
+            error_person_name = true;
+            return;
+        }
+    }
+
+    //验证库名字是否为空
+    function check_base_name() {
+        var val = $('#base_id').val();
+        if(val == ''){
+            error_base_name = true;
+            return;
         }
     }
 
 
-    $('form').submit(function () {
-        check_why();
-        check_use();
-        if (error_why == false && error_use == false) {
+    $('form').submit(function(){
+        check_group_name();
+        check_group_abb();
+        check_person_name();
+        check_base_name();
+        if(error_group_name == false && error_group_abb == false && error_person_name == false && error_base_name == false){
             alert('申请成功');
         }
-        else {
+        else{
             alert('请输入正确的申请');
+            //刷新页面
             window.location.reload();
             return false;
         }
